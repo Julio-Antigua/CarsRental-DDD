e0c098d8f2f538afb065868c99e1330c49263cdd
Julio Saul <juliosaul12@gmail.com>
2024-09-13 10:28:03 -0400

    IServiceColection(DepencyInjection) was created

---
+++ CarsRental.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CarsRental.Domain.Services.Rentals;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarsRental.Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration => {
+                configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PriceService>();
+
+            return services;
+        }
+    }
+}
