741324b54668e2f12322ffabd4b7c3281691fa21
Julio Saul <juliosaul12@gmail.com>
2024-09-13 10:25:37 -0400

    IEmailServices was created

---
+++ CarsRental.Application/Abstractions/Emails/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using CarsRental.Domain.ObjectsValue.Users;
+
+namespace CarsRental.Application.Abstractions.Emails
+{
+    public interface IEmailService
+    {
+        Task SendAsync(Email recipent, string subejct, string body);
+    }
+}
